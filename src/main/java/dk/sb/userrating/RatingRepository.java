e5bc017220a42b06e6a49fe82e709ff106adddce
Simon Schønberg Bojesen <cph-sb339@cphbusiness.dk>
2020-12-01 16:13:28 +0100

    added the needed functionality

--- src/main/java/dk/sb/userrating/RatingRepository.java
+++ src/main/java/dk/sb/userrating/RatingRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Collection;
 
 public interface RatingRepository extends MongoRepository<Rating, String> {
-    Collection<Rating> findAllByCar(int id);
+    Collection<Rating> findAllByCar(Long id);
     Collection<Rating> findAllByUser(User user);
 }
--- src/main/java/dk/sb/userrating/UserratingApplication.java
+++ src/main/java/dk/sb/userrating/UserratingApplication.java
@@ -5,9 +5,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.Collection;
 
+@EnableHystrixDashboard
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class UserratingApplication implements CommandLineRunner {
 
